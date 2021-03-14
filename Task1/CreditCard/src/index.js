940945f1f084815b07966cf237bb499d9ecb87c0
ElMagdo <magdoe19@gmail.com>
2021-03-14 12:04:23 +0100

    Added the cardInfo prop.

--- Task1/CreditCard/src/index.js
+++ Task1/CreditCard/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-function CreditCard() {
+function CreditCard({cardInfo}) {
     return(
         <div className="card">NO CREDIT!!....</div>
     );
 }
 
-ReactDOM.render(<CreditCard/>, document.querySelector("#root"));
+let info = {
+    bankName: "Big Bank, Inc",
+    cardNum: "1234 5678 8765 4321",
+    expDate: "08/19",
+    personName: "CARDHOLDER NAME"
+}
+
+ReactDOM.render(<CreditCard cardInfo={info}/>, document.querySelector("#root"));
