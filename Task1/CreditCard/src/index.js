49aaa8f0a7e2c4f3222df6fb7e8b998570930278
ElMagdo <magdoe19@gmail.com>
2021-03-14 11:38:48 +0100

    Created and rendered a starting component.

--- Task1/CreditCard/src/index.js
+++ Task1/CreditCard/src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function CreditCard() {
+    return(
+        <div className="card">NO CREDIT!!....</div>
+    );
+}
+
+ReactDOM.render(<CreditCard/>, document.querySelector("#root"));
