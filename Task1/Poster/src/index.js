bae8168928d1fc3b0ad64f9fb3eb8fc02e176453
ElMagdo <magdoe19@gmail.com>
2021-03-14 23:14:33 +0100

    Added proptypes for Poster component.

--- Task1/Poster/src/index.js
+++ Task1/Poster/src/index.js
@@ -9,6 +9,12 @@ function Poster({image, title, text}) {
     );
 }
 
+Poster.propTypes = {
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired
+}
+
 let img = "./post.png";
 let hdx = "REACT";
 let txt = "The best thing since jQuery, probably";
