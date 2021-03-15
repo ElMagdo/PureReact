e1e4b4ac3902d2e54d936ccf75709dbfe1876727
ElMagdo <magdoe19@gmail.com>
2021-03-15 20:42:55 +0100

    Created sample Email component and rendered it.

--- Task1/Email/src/index.js
+++ Task1/Email/src/index.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function Email() {
+    return (
+        <div className="email">YOUR NEW EMAIL (%^%)</div>
+    );
+}
+
+ReactDOM.render(<Email/>, document.querySelector("#root"));
