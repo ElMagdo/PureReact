c64db0d9adbf14bf66814b4382e312727d0a5a3e
ElMagdo <magdoe19@gmail.com>
2021-03-15 21:22:49 +0100

    Created and added email prop to Email component.

--- Task1/Email/src/index.js
+++ Task1/Email/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-function Email() {
+function Email(email) {
     return (
         <div className="email">YOUR NEW EMAIL (%^%)</div>
     );
 }
 
-ReactDOM.render(<Email/>, document.querySelector("#root"));
+let content = {
+    sender: "React Newsletter",
+    subject: "Issue 36",
+    date: "Jul 15",
+    message: "th 2016 Read this issue on the web http://reactjsnewsletter.com/issues/36?sid=3QGDYBX ### Comme"
+};
+
+ReactDOM.render(<Email email={content}/>, document.querySelector("#root"));
