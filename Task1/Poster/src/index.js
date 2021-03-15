68c43a6bda8aaa2380c98baf0f235907f660ae4f
ElMagdo <magdoe19@gmail.com>
2021-03-15 14:02:37 +0100

    Created the Poster component.

--- Task1/Poster/src/index.js
+++ Task1/Poster/src/index.js
@@ -7,6 +7,7 @@ function Poster({image, title, text}) {
     return (
         <div className="poster">
             <Image src={image}/>
+            <Title tag={title}/>
         </div>
     );
 }
@@ -21,8 +22,16 @@ let img = "./post.png";
 let hdx = "REACT";
 let txt = "The best thing since jQuery, probably";
 
-let Image = (src) => (
+let Image = ({src}) => (
 <div className="image"><img src={src} alt="Poster"/></div>
+);
+
+let Title = ({tag}) => (
+<div className="title">
+    <span className="first">{tag[0]}</span>
+    {tag.slice(1, tag.length-1)}
+    <span className="last">{tag[tag.length-1]}</span>
+</div>
 );
 
 ReactDOM.render(<Poster image={img} title={hdx} text={txt}/>, document.querySelector("#root"));
