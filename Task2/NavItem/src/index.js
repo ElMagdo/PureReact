38044fcc58642df5ac1c5273c03fffce9294d316
ElMagdo <magdoe19@gmail.com>
2021-03-17 22:13:38 +0100

    Created the Nav component.

--- Task2/NavItem/src/index.js
+++ Task2/NavItem/src/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function Nav() {
+    return (
+        <div className>Links coming soon</div>
+    );
+}
+
+ReactDOM.render(
+    <Nav/>,
+    document.querySelector("#root")
+);
