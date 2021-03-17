b17ecbcc7f8ef2b6018c60a8d052093a455c18c3
ElMagdo <magdoe19@gmail.com>
2021-03-17 14:50:25 +0100

    Created ErrorBox component and rendered it.

--- Task2/ErrorBox/src/index.js
+++ Task2/ErrorBox/src/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function ErrorBox() {
+    return (
+        <div className="err-box">
+            <i className="fa fa-star"/>
+            Wait for the ERROR (@-@)
+        </div>
+    );
+}
+
+ReactDOM.render(<ErrorBox/>, document.querySelector("#root"));
