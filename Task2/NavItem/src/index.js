af4caae990006863859cbd3f1fe6336a7da20579
ElMagdo <magdoe19@gmail.com>
2021-03-18 12:31:59 +0100

    Added the children and NavItem component.

--- Task2/NavItem/src/index.js
+++ Task2/NavItem/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Nav() {
+function Nav({children}) {
     return (
-        <div className>Links coming soon</div>
+        <div className="nav-ui">{children}</div>
     );
 }
 
+let NavItem = ({url, children}) => <a href={url}> {children} </a>;
+
 ReactDOM.render(
-    <Nav/>,
+    <Nav>
+        <NavItem url="/"> Home </NavItem>
+        <NavItem url="/Contact"> Contact </NavItem>
+        <NavItem url="/About"> About </NavItem>
+    </Nav>,
     document.querySelector("#root")
 );
