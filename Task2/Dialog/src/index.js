dd981a20f6c2a3d543a0764d14334cbaffccfe6b
ElMagdo <magdoe19@gmail.com>
2021-03-20 02:13:52 +0100

    Created and rendered a sample DialogBox component.

--- Task2/Dialog/src/index.js
+++ Task2/Dialog/src/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function DialogBox() {
+    return (
+        <div className="dlg">Lets Start DIALOGING (*=*) ......</div>
+    );
+}
+
+ReactDOM.render(
+    <DialogBox/>,
+    document.querySelector("#root")
+);
