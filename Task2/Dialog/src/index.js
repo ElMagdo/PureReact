243adfc793c03c7437bde480721c7f2241ad4921
ElMagdo <magdoe19@gmail.com>
2021-03-20 18:02:16 +0100

    Created and rendered Title component.

--- Task2/Dialog/src/index.js
+++ Task2/Dialog/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-function DialogBox() {
+function DialogBox({children}) {
     return (
-        <div className="dlg">Lets Start DIALOGING (*=*) ......</div>
+        <div className="dlg">{children}</div>
     );
 }
+let hdx = "This Is Important";
+let Title = ({title}) => <div className="title">{title}</div>;
 
 ReactDOM.render(
-    <DialogBox/>,
+    <DialogBox>
+        <Title title={hdx}/>
+    </DialogBox>,
     document.querySelector("#root")
 );
