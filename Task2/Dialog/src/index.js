fdd29d187ab2be4aea0d69ef7fca95de2ef3b53b
ElMagdo <magdoe19@gmail.com>
2021-03-20 18:11:56 +0100

    Created and rendered Body component.

--- Task2/Dialog/src/index.js
+++ Task2/Dialog/src/index.js
@@ -10,9 +10,13 @@ function DialogBox({children}) {
 let hdx = "This Is Important";
 let Title = ({title}) => <div className="title">{title}</div>;
 
+let txt = "Here is an important text or error or something";
+let Body = ({body}) => <div className="bdy">{body}</div>;
+
 ReactDOM.render(
     <DialogBox>
         <Title title={hdx}/>
+        <Body body={txt}/>
     </DialogBox>,
     document.querySelector("#root")
 );
