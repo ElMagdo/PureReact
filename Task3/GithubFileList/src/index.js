503655829bdb0bd6e5a1e59f3c78f11831145d4c
ElMagdo <magdoe19@gmail.com>
2021-03-22 12:18:08 +0100

    Created and rendered the FileList component.

--- Task3/GithubFileList/src/index.js
+++ Task3/GithubFileList/src/index.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function FileList() {
+    return (
+        <table className="file-list">
+            <tbody><tr><td>Be ready to commit</td></tr></tbody>
+        </table>
+    );
+}
+
+ReactDOM.render(
+    <FileList/>,
+    document.querySelector("#root")
+);
