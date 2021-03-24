b6d499764f8096853a02867810023d9787d32835
ElMagdo <magdoe19@gmail.com>
2021-03-24 23:41:57 +0100

    Adjusted the various time stamps.

--- Task3/GithubFileList/src/index.js
+++ Task3/GithubFileList/src/index.js
@@ -21,7 +21,7 @@ const testFiles = [{
     id: 1,
     name: 'build',
     type: 'folder',
-    updated_at: "2016-07-11 21:24:00",
+    updated_at: "2021-01-11 21:24:00",
     latestCommit: {
         message: 'Close #1687, replace es3ify with Babel ES3 tranforms (#1688)'
     }
@@ -30,7 +30,7 @@ const testFiles = [{
     id: 2,
     name: 'docs',
     type: 'folder',
-    updated_at: "2016-07-11 21:24:00",
+    updated_at: "2021-03-24 21:24:00",
     latestCommit: {
         message: 'Mention that we\'re Observable in the API.'
     }
@@ -39,7 +39,7 @@ const testFiles = [{
     id: 3,
     name: 'examples',
     type: 'folder',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2021-03-06 21:24:00",
     latestCommit: {
         message: 'Update doc to test with Enzyme (#1692)'
     }
@@ -48,7 +48,7 @@ const testFiles = [{
     id: 4,
     name: 'logos',
     type: 'folder',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2021-01-18 21:24:00",
     latestCommit: {
         message: 'Use Redux logo as favicon on GitBook docs (#1761)'
     }
@@ -57,7 +57,7 @@ const testFiles = [{
     id: 5,
     name: 'src',
     type: 'folder',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2021-03-02 21:24:00",
     latestCommit: {
         message: 'Only warn for unexpected once per key (#1818)'
     }
@@ -66,7 +66,7 @@ const testFiles = [{
     id: 6,
     name: 'test',
     type: 'folder',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2021-03-02 21:24:00",
     latestCommit: {
         message: 'Only warn for unexpected once per key (#1818)'
     }
@@ -75,7 +75,7 @@ const testFiles = [{
     id: 7,
     name: '.babelrc',
     type: 'file',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2021-01-18 21:24:00",
     latestCommit: {
         message: 'Close #1687, replace es3ify with Babel ES3 tranforms (#1688)'
     }
@@ -84,7 +84,7 @@ const testFiles = [{
     id: 8,
     name: '.editorconfig',
     type: 'file',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2020-10-18 21:24:00",
     latestCommit: {
         message: 'editorconfig: do not trim trailing spaces in Markdown files'
     }
@@ -93,7 +93,7 @@ const testFiles = [{
     id: 9,
     name: '.eslintignore',
     type: 'file',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2020-11-18 21:24:00",
     latestCommit: {
         message: 'Really ignore all node_modules and dist in eslint.'
     }
@@ -102,7 +102,7 @@ const testFiles = [{
     id: 10,
     name: '.eslintrc',
     type: 'file',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2020-06-18 21:24:00",
     latestCommit: {
         message: 'Bump eslint version'
     }
@@ -111,7 +111,7 @@ const testFiles = [{
     id: 11,
     name: '.flowconfig',
     type: 'file',
-    updated_at: "2016-07-18 21:24:00",
+    updated_at: "2020-01-18 21:24:00",
     latestCommit: {
         message: 'Add Flow type annotations'
     }
