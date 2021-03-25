0d06efd6ce6da2766a2bfadba1b78d90393f6007
ElMagdo <magdoe19@gmail.com>
2021-03-25 14:08:53 +0100

    Created and exported the Icon component.

--- Task3/GithubFileList-2/src/icon-type.js
+++ Task3/GithubFileList-2/src/icon-type.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+function Icon({type}) {
+    let icon = (type === "folder") ? "fa-folder" : "fa-file-text-o";
+
+    return <span className="icon"><i className={`fa ${icon}`}/></span>;
+}
+
+export default Icon;
