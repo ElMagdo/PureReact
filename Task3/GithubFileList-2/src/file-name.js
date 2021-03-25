86dceef3007b6dbfeccd5dbe03443986249d1f49
ElMagdo <magdoe19@gmail.com>
2021-03-25 18:02:52 +0100

    Added prop-types for the Name component.

--- Task3/GithubFileList-2/src/file-name.js
+++ Task3/GithubFileList-2/src/file-name.js
@@ -3,4 +3,8 @@ import PropTypes from 'prop-types';
 
 let Name = ({name}) => <span className="name">{name}</span>;
 
+Name.propTypes = {
+    name: PropTypes.string.isRequired
+}
+
 export default Name;
