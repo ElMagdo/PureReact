0b70fc463e17a19b2100e3e33515328165b4afe5
ElMagdo <magdoe19@gmail.com>
2021-03-25 09:00:41 +0100

    Created, initialised and Rendered FileList component.

--- Task3/GithubFileList-2/src/index.js
+++ Task3/GithubFileList-2/src/index.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+function FileList() {
+    return <div>Repo | Project | About | Commit</div>;
+}
+
+ReactDOM.render(
+    <FileList/>,
+    document.querySelector("#root")
+);
