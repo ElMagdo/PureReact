f814dfd3a4ea47dd422e53c257e69eea1c1af2df
ElMagdo <magdoe19@gmail.com>
2021-03-25 18:09:42 +0100

    Added prop-types for the Message component.

--- Task3/GithubFileList-2/src/message.js
+++ Task3/GithubFileList-2/src/message.js
@@ -3,4 +3,8 @@ import PropTypes from 'prop-types';
 
 let Message = ({commit}) => <div className="commit-msg">{commit.message}</div>;
 
+Message.propTypes = {
+    commit: PropTypes.object.isRequired
+}
+
 export default Message;
