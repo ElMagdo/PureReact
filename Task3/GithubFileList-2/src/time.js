7efafe741a7ed2583d1cbc596df283d4091d9c36
ElMagdo <magdoe19@gmail.com>
2021-03-25 18:14:03 +0100

    Created and exported the Time component.

--- Task3/GithubFileList-2/src/time.js
+++ Task3/GithubFileList-2/src/time.js
@@ -2,3 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
+const Time = ({ time }) => {
+  const timeString = moment(time).fromNow();
+  return <div className="time">{timeString}</div>;
+};
+
+export default Time;
