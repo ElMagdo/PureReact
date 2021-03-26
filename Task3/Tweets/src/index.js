3bbb07d55540e348148a77f498614a1c86d18df0
ElMagdo <magdoe19@gmail.com>
2021-03-26 14:14:50 +0100

    Created and rendered the Tweet component.

--- Task3/Tweets/src/index.js
+++ Task3/Tweets/src/index.js
@@ -2,3 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './style.css';
+
+function Tweets() {
+    return <div className="tweets">Tweeter Loading... </div>
+}
+
+ReactDOM.render(
+    <Tweets/>,
+    document.querySelector("#root")
+);
