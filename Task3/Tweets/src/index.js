0dab564a7bd0c63dee2d7552ba15ef04b7d89fed
ElMagdo <magdoe19@gmail.com>
2021-03-26 14:36:39 +0100

    Adde the testTweets array for editing.

--- Task3/Tweets/src/index.js
+++ Task3/Tweets/src/index.js
@@ -7,6 +7,74 @@ function Tweets() {
     return <div className="tweets">Tweeter Loading... </div>
 }
 
+var testTweets = [
+{
+    message: "Something about cats.",
+    gravatar: "xyz",
+    author: {
+        handle: "catperson",
+        name: "IAMA Cat Person"
+    },
+    buttons: {
+        likes: 2,
+        retweets: 0,
+        timestamp: "2016-07-30 21:24:37"
+    }
+},
+{
+    message: "Something about cats.",
+    gravatar: "xyz",
+    author: {
+        handle: "catperson",
+        name: "IAMA Cat Person"
+    },
+    buttons: {
+        likes: 2,
+        retweets: 0,
+        timestamp: "2016-07-30 21:24:37"
+    }
+},
+{
+    message: "Something about cats.",
+    gravatar: "xyz",
+    author: {
+        handle: "catperson",
+        name: "IAMA Cat Person"
+    },
+    buttons: {
+        likes: 2,
+        retweets: 0,
+        timestamp: "2016-07-30 21:24:37"
+    }
+},
+{
+    message: "Something about cats.",
+    gravatar: "xyz",
+    author: {
+        handle: "catperson",
+        name: "IAMA Cat Person"
+    },
+    buttons: {
+        likes: 2,
+        retweets: 0,
+        timestamp: "2016-07-30 21:24:37"
+    }
+},
+{
+    message: "Something about cats.",
+    gravatar: "xyz",
+    author: {
+        handle: "catperson",
+        name: "IAMA Cat Person"
+    },
+    buttons: {
+        likes: 2,
+        retweets: 0,
+        timestamp: "2016-07-30 21:24:37"
+    }
+}
+];
+
 ReactDOM.render(
     <Tweets/>,
     document.querySelector("#root")
