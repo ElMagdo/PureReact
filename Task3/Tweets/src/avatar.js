a55c436f28cb644d487ce88e4b9cb2b5b46e43a5
ElMagdo <magdoe19@gmail.com>
2021-03-26 19:31:20 +0100

    Created and exported the Avatar component.

--- Task3/Tweets/src/avatar.js
+++ Task3/Tweets/src/avatar.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const Avatar = () => (
+    <div className="avatar">
+        <i className="fa fa-home"/>
+    </div>
+);
+
+export default Avatar;
