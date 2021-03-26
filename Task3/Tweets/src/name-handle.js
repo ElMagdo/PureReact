ec5a147c5d471c786cd26402373cdca66929f61f
ElMagdo <magdoe19@gmail.com>
2021-03-26 21:20:24 +0100

    Added the prop-types to the NameHandle component.

--- Task3/Tweets/src/name-handle.js
+++ Task3/Tweets/src/name-handle.js
@@ -4,8 +4,15 @@ import PropTypes from 'prop-types';
 const NameHandle = ({user}) => (
 <span className="name-handle">
     <span className="name">{user.name}</span>
-    <span className="handle">{user.handle}</span>
+    <span className="handle">@{user.handle}</span>
 </span>
 );
+
+NameHandle.propTypes = {
+    user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        handle: PropTypes.string.isRequired
+    }).isRequired
+}
 
 export default NameHandle;
