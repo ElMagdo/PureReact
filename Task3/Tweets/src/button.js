1eda4af499de4658bee134c94329ec73a856d288
ElMagdo <magdoe19@gmail.com>
2021-03-26 22:54:34 +0100

    Created and rendered RetweetButton component.

--- Task3/Tweets/src/button.js
+++ Task3/Tweets/src/button.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 const Buttons = ({buttons}) => (
 <div className="buttons">
     <ReplyButton/>
+    <RetweetButton/>
+    <MoreOptionsButton/>
 </div>
 );
 
@@ -12,6 +14,10 @@ Buttons.propTypes = {
 }
 
 const ReplyButton = () => <i className="fa fa-reply reply-button"/>;
+
+const RetweetButton = () => (
+<i className="fa fa-retweet retweet-button"/>
+);
 
 const MoreOptionsButton = () => <i className="fa fa-ellipsis-h more-options-button"/>;
 
