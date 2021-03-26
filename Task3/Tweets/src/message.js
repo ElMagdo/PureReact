ac19025e44a035736fec224b15dfd9305af69687
ElMagdo <magdoe19@gmail.com>
2021-03-26 20:36:47 +0100

    Added prop-types for the Message component.

--- Task3/Tweets/src/message.js
+++ Task3/Tweets/src/message.js
@@ -3,4 +3,8 @@ import PropTypes from 'prop-types';
 
 const Message = ({text}) => <div className="msg">{text}</div>;
 
+Message.propTypes = {
+    text: PropTypes.string.isRequired
+}
+
 export default Message;
