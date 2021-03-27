890c6762138966f2d5c5f7a168380864876b0219
ElMagdo <magdoe19@gmail.com>
2021-03-27 20:30:46 +0100

    Created and rendered the Board component.

--- Task3/Trello/src/index.js
+++ Task3/Trello/src/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+
+let Board = () => <div className="board">Trello coming soon</div>;
+
+ReactDOM.render(
+    <Board/>,
+    document.querySelector("#root")
+);
