c2336b79eae0d2cbe0c8772682cb9e2df1ed5df8
ElMagdo <magdoe19@gmail.com>
2021-03-27 21:22:20 +0100

    Created the testCards array to hold the card list items.

--- Task3/Trello/src/index.js
+++ Task3/Trello/src/index.js
@@ -4,6 +4,25 @@ import PropTypes from 'prop-types';
 
 let Board = () => <div className="board">Trello coming soon</div>;
 
+var testCards = [
+{
+    id: 0,
+    head: "Phone Features",
+    list: [
+        "Subwoofer", "Non-porous, washable", "Wings",
+        "Beveled Bezel", "Bezeled Bevel", "Seedless"
+    ]
+},
+{
+    id: 1,
+    head: "Phone Features",
+    list: [
+        "Subwoofer", "Non-porous, washable", "Wings",
+        "Beveled Bezel", "Bezeled Bevel", "Seedless"
+    ]
+}
+];
+
 ReactDOM.render(
     <Board/>,
     document.querySelector("#root")
