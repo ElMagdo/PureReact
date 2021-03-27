908ff68086810e637d52c1b3cd45c654febb178f
ElMagdo <magdoe19@gmail.com>
2021-03-27 23:16:47 +0100

    Added prop-types to the Item component.

--- Task3/Trello/src/item.js
+++ Task3/Trello/src/item.js
@@ -3,4 +3,8 @@ import PropTypes from 'prop-types';
 
 const Item = ({item}) => <div className="item">{item}</div>;
 
+Item.propTypes = {
+    item: PropTypes.string.isRequired
+}
+
 export default Item;
