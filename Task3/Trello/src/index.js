9026172eadc9929ed1db932e3cb13cde5b73fafa
ElMagdo <magdoe19@gmail.com>
2021-03-27 22:43:38 +0100

    Imported the Title component.

--- Task3/Trello/src/index.js
+++ Task3/Trello/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import Title from './title';
 
 function Board({children}) {
     return <div className="board">{children}</div>;
