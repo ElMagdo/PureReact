618b22affa2a6aeca83abe734e997ffd94b94ec7
ElMagdo <magdoe19@gmail.com>
2021-03-28 21:50:01 +0100

    Updated the Post component prop-types.

--- Task3/HackerNews/src/post.js
+++ Task3/HackerNews/src/post.js
@@ -6,7 +6,15 @@ const Post = ({feed}) => (
 );
 
 Post.propTypes = {
-    feed: PropTypes.object.isRequired
+    feed: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        site: PropTypes.string.isRequired,
+        points: PropTypes.number.isRequired,
+        writer: PropTypes.string.isRequired,
+        posted_at: PropTypes.string.isRequired,
+        comments: PropTypes.number.isRequired
+    }).isRequired
 }
 
 export default Post;
