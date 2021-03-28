36b46f341a7f676acef3c09ddfe00f79d143f2a1
ElMagdo <magdoe19@gmail.com>
2021-03-28 19:33:22 +0100

    Added more objects to the testNews array.

--- Task3/HackerNews/src/index.js
+++ Task3/HackerNews/src/index.js
@@ -16,6 +16,87 @@ var testNews = [
         writer: "Ivank",
         posted_at: "2021-03-28 19:29",
         comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    },
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
     }
 ];
 
