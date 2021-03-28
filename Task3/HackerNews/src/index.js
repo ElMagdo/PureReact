dc8e424a43a862f90edc55cbbfc27b0622b250b0
ElMagdo <magdoe19@gmail.com>
2021-03-28 19:31:18 +0100

    Created the testNews array.

--- Task3/HackerNews/src/index.js
+++ Task3/HackerNews/src/index.js
@@ -7,6 +7,18 @@ const View = () => (
     <div className="view">The latest coming up...</div>
 );
 
+var testNews = [
+    {
+        id: 0,
+        title: "Why I'm Suing the US Government",
+        site: "bunniestudios.com",
+        points: 1346,
+        writer: "Ivank",
+        posted_at: "2021-03-28 19:29",
+        comments: 257
+    }
+];
+
 ReactDOM.render(
     <View/>,
     document.querySelector("#root")
