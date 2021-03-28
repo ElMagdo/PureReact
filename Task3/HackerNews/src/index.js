9ea0a5cd64b3a06ff40ff154ed25245e598b7e56
ElMagdo <magdoe19@gmail.com>
2021-03-28 21:35:50 +0100

    Imported Post component.

--- Task3/HackerNews/src/index.js
+++ Task3/HackerNews/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './style.css';
 import Navbar from './navbar';
+import Post from './post';
 
 const View = ({news}) => (
     <div className="view">
