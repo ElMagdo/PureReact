4d649a5c96635804efc8bfeee29014bedb9d1072
ElMagdo <magdoe19@gmail.com>
2021-03-28 15:46:28 +0100

    Created and rendered the View component.

--- Task3/HackerNews/src/index.js
+++ Task3/HackerNews/src/index.js
@@ -2,3 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './style.css';
+
+const View = () => (
+    <div className="view">The latest coming up...</div>
+);
+
+ReactDOM.render(
+    <View/>,
+    document.querySelector("#root")
+);
