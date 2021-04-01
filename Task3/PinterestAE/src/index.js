5af243d1b7db00622a372b420623c1c31cf3098b
ElMagdo <magdoe19@gmail.com>
2021-04-01 16:25:26 +0100

    Created and rendered Explorer component.

--- Task3/PinterestAE/src/index.js
+++ Task3/PinterestAE/src/index.js
@@ -2,3 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './style.css';
+
+function Explorer() {
+    return <div className="avi-exp">Cool page</div>;
+}
+
+ReactDOM.render(
+    <Explorer/>,
+    document.querySelector("#root")
+);
