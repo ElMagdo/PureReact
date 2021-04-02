1f10adc12fc175c6886aac939aa7e97ac58acff0
ElMagdo <magdoe19@gmail.com>
2021-04-02 21:16:42 +0100

    Added prop-types for Card component.

--- Task3/PinterestAE/src/card.js
+++ Task3/PinterestAE/src/card.js
@@ -5,4 +5,14 @@ const Card = ({body}) => (
     <div className="card">I'm one of me</div>
 );
 
+Card.propTypes = {
+    body: PropTypes.arrayOf(PropTypes.shape({
+        caption: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        cap_img: PropTypes.string.isRequired,
+        ls_img: PropTypes.array.isRequired,
+        pins: PropTypes.number.isRequired
+    }))
+}
+
 export default Card;
